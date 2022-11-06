0aab2a42dd54ad0e1a14d55b71ff4d8d8e902df8
msultanovdev <mukhammadamin.sultanov@gmail.com>
2022-11-06 20:23:38 +0500

    feat: add 03-files-in-folder problem

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs'),
+      path = require('path'),
+      fsPromises = fs.promises;
+let dirpath = path.join(__dirname, 'secret-folder');
+
+fs.promises.readdir((dirpath), {
+    withFileTypes: true
+}).then(results => {
+    results.map(item => {
+        if(!item.isDirectory()) {
+            const filePath = path.join(dirpath, item.name),
+              fileName = path.basename(filePath),
+              ext = path.extname(filePath);
+              fsPromises.stat(filePath).then(res => {
+                console.log(`${fileName.replace(ext, '')} | ${ext.replace('.', '')} | ${Number(res.size / 2000).toFixed(3)}kb`);
+              });
+        }
+    });
+})
