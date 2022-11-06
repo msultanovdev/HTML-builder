bafc5254337534052e9ba6738c86786b27340df2
msultanovdev <mukhammadamin.sultanov@gmail.com>
2022-11-06 22:42:17 +0500

    feat: solve 04-copy-dir solition

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs'),
+      path = require('path'),
+      fsPromises = fs.promises;
 
+copyFolder();
+
+function copyFolder() {
+    fs.mkdir(path.join(__dirname, 'files-copy'), {
+        recursive: true,
+    }, err => {
+        if(err) {
+           throw new Error('Папка с таким названием существует или что-то пошло не так!'); 
+        }
+        console.log('Папка успешно склонирована!');
+    });
+    fsPromises.readdir(path.join(__dirname, 'files')).then(items => {
+        items.forEach(item => {
+            const filePath = path.join(__dirname, 'files', item);
+            fsPromises.copyFile(filePath, path.join(__dirname, 'files-copy', item));
+            console.log(item);
+        });
+    });
+};
